124a472688a7e723680e75be848c94f1cb38d511
konman1989 <manoilo.blog@gmail.com>
2019-09-26 07:35:40 +0300

    changes implemented

--- lesson4.py
+++ lesson4.py
@@ -1,75 +1,75 @@
-# names = ['Janie', 'Daniel', 'Steve', 'Bob', 'Rob']
-#
-# print(len(names))
-# names.append('David')
-# print(names[::-1])
-# names.insert(0, 'Victor')   #add string with a given index
-# names.remove('Daniel')      #removes first match string
-# del names[5]                #removes particular index in the list
-# names.reverse()             #reversed the list
-# names.sort()                #sorts in alphabet order by default or numbers
-#
-# print(names)
-#
-# print('Victor' in names)    #returns True or falls if exists in the list
-#
-# sentence = 'Hi, my name is Steve'
-# words = sentence.split(' ')
-# print(len(words))
-#
-# sentence2 = 'Hi-my-name-is-Karl'
-# words2 = sentence2.split('-')
-# print(words2)
-#
-# sentence3 = ', '.join(names)    #', ' - is a template how to concatenate
-# print(sentence3)
-#
-# print(names.index('Steve'))    #prints string index
-#
-# name1 = names[1]
-# name2 = names[3]
-#
-# print(name1, name2)
-#
-# name4 = names.pop()             #removes the last str and evaluates to a new
-#                                 #variable (vs .remove)
-# print(names)
-# names.append('Karl')
-# names2 = names
-# print(names is names2)
-#
-# print(names)
-# print(names2)
-#
-# names3 = names.copy()         #makes a copy of the existing list
-# print(names3 is names)
-#
-# #.copy and deepcopy!!!
-#
-# nm = ['bob', 'rob', 'john']
-# nm2 = nm.copy()
-#
-# print(nm2)
-#
-# -------------------------------------------TUPLES
-# names = ['Jane', 'Steve', 'Robert']
-# print(type(names))
-#
-# names2 = ('Jane', 'Steve', 'Robert')
-# print(type(names2))
-#
-# names3 = list(names2)       #creates a list variable of a tuple
-#
-# a = 15
-# b = 34
-# a, b = b, a                #swaps variables
-#
-# name = 'Steve'
-# names = name,              #creating a tuple by using a coma
-# print(type(names))
+names = ['Janie', 'Daniel', 'Steve', 'Bob', 'Rob']
 
-#Task
-# f - strings
+print(len(names))
+names.append('David')
+print(names[::-1])
+names.insert(0, 'Victor')   #add string with a given index
+names.remove('Daniel')      #removes first match string
+del names[5]                #removes particular index in the list
+names.reverse()             #reversed the list
+names.sort()                #sorts in alphabet order by default or numbers
+
+print(names)
+
+print('Victor' in names)    #returns True or falls if exists in the list
+
+sentence = 'Hi, my name is Steve'
+words = sentence.split(' ')
+print(len(words))
+
+sentence2 = 'Hi-my-name-is-Karl'
+words2 = sentence2.split('-')
+print(words2)
+
+sentence3 = ', '.join(names)    #', ' - is a template how to concatenate
+print(sentence3)
+
+print(names.index('Steve'))    #prints string index
+
+name1 = names[1]
+name2 = names[3]
+
+print(name1, name2)
+
+name4 = names.pop()             #removes the last str and evaluates to a new
+                                #variable (vs .remove)
+print(names)
+names.append('Karl')
+names2 = names
+print(names is names2)
+
+print(names)
+print(names2)
+
+names3 = names.copy()         #makes a copy of the existing list
+print(names3 is names)
+
+#.copy and deepcopy!!!
+
+nm = ['bob', 'rob', 'john']
+nm2 = nm.copy()
+
+print(nm2)
+
+# -------------------------------------------TUPLES
+names = ['Jane', 'Steve', 'Robert']
+print(type(names))
+
+names2 = ('Jane', 'Steve', 'Robert')
+print(type(names2))
+
+names3 = list(names2)       #creates a list variable of a tuple
+
+a = 15
+b = 34
+a, b = b, a                #swaps variables
+
+name = 'Steve'
+names = name,              #creating a tuple by using a coma
+print(type(names))
+
+Task
+f - strings
 
 def tag_add(sentence, tag):
     html = f"<{tag}>{sentence}</{tag}>"
